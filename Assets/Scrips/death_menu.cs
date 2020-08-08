a7b0dccd64ebd9eeef6d12c77d11c4cc9c4165de
vuotnguyen <nguyenvanvuot.thuanphat@gmail.com>
2020-08-08 14:34:00 +0700

    lab7_playgame

--- Assets/Scrips/death_menu.cs
+++ Assets/Scrips/death_menu.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class death_menu : MonoBehaviour
 {
+    public Text scoreText;
+    public Image background;
+    public bool isShow = false;
+    public float transition = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +20,19 @@ public class death_menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(!isShow)
+        return;
+        transition += Time.deltaTime;
+        background.color = Color.Lerp(new Color(0,0,0,0),Color.black,transition);
     }
     public void toggleMenu(float score){
         gameObject.SetActive(true);
+        scoreText.text  = ((int)score).ToString();
+        isShow = true;
     }
+    public void reStart(){
+        //ScenceManager.LoadScene("SampleScene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
 }
